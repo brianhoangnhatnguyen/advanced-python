e314bfe3272694e339dc38fb2895f525c2e4db93
skibididopdopdopbrrr <asdfasdfasdfsadf098234502023@gmail.com>
2023-08-11 19:18:01 -0500

    commit

---
+++ class1.py
@@ -0,0 +1,35 @@
+# Inputs:
+"""name = input("Please enter your name: ")
+print("Hello, " + name + ".")
+num1 = int(input("Enter in a number: "))
+num2 = int(input("Enter in another number: "))
+sum = num1 + num2
+print("Here is your sum:", sum)"""
+
+# Functions:
+"""def sum(num1, num2):
+    return num1 + num2
+num1 = 5
+num2 = 6
+addition = sum(num1, num2)
+print(addition)"""
+
+# Fixed value:
+"""addition = sum(5, 6)
+print(addition)"""
+
+# User input:
+"""def sumInput(num1, num2):
+    return num1 + num2
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+addition = sumInput(num1, num2)
+print(addition)"""
+
+# Area of a rectangle:
+def area(l, w):
+    return l * w
+l = int(input("L: "))
+w = int(input("W: "))
+res = area(l, w)
+print(res)
