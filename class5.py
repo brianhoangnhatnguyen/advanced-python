0e435ce8d953c71cec89a0946b98124214933649
skibididopdopdopbrrr <asdfasdfasdfsadf098234502023@gmail.com>
2023-08-31 20:27:27 -0500

    Object Oriented Programming

--- class5.py
+++ class5.py
@@ -30,3 +30,26 @@ class samsung(iphone):
         print("android device")
 
 sam = samsung()
+
+class Shape:
+    def __init__(self, dim1, dim2):
+        self.dim1 = dim1
+        self.dim2 = dim2
+
+    def area(self):
+        print("This is a super class method")
+
+class Triangle(Shape):
+    def area(self):
+        print(self.dim1 * self.dim2 * 0.5)
+
+
+class Rectangle(Shape):
+    def area(self):
+        print(self.dim1 * self.dim2)
+
+shape1 = Triangle(10, 20)
+shape1.area()
+
+shape2 = Rectangle(30, 100)
+shape2.area()
