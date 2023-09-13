4a36fa038123545a4fe39acf02ebb819951bd400
skibididopdopdopbrrr <asdfasdfasdfsadf098234502023@gmail.com>
2023-09-12 21:42:38 -0500

    imports

---
+++ num.py
@@ -0,0 +1,19 @@
+import numpy
+import matplotlib as plot
+
+speed = [34, 72, 19, 53, 65, 42, 87, 27, 60, 14, 95, 78, 46, 23, 68, 11, 88, 37, 99, 50]
+x = numpy.mean(speed)
+print(x)
+
+y = numpy.median(speed)
+print(y)
+
+p = numpy.std(speed)
+print(p)
+
+something = [1,1,1,2,3,7,4,9,2,8,5,8,34,7,43,7,43,78,34,7,4,78,43,7,4,8,6,3,7,9,8,43,4,7,98,65,4]
+
+plot.hist(something)
+plot.xlabel("value")
+plot.ylabel("value")
+plot.show()
